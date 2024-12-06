ef52318379210ddc4b45f1da3ce42e66c72a7761
TomWilkRavoux <tomwilkwilk@gmail.com>
2024-12-06 15:48:05 +0100

    Ajout d'un test pour les liens

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,5 +1,8 @@
 from playwright.sync_api import sync_playwright
 
+BASE_URL = "http://127.0.0.1:5500"
+
+
 def test_page_loads():
     with sync_playwright() as p:
         browser = p.chromium.launch()
@@ -7,7 +10,7 @@ def test_page_loads():
         
 
         # page.goto("http://127.0.0.1:5500/wwwroot")
-        page.goto("http://127.0.0.1:5500", wait_until="networkidle")
+        page.goto(BASE_URL, wait_until="networkidle")
 
         
         
@@ -26,7 +29,7 @@ def test_menu_links():
         
         
         # page.goto("http://127.0.0.1:5500/wwwroot")
-        page.goto("http://127.0.0.1:5500", wait_until="networkidle")
+        page.goto(BASE_URL, wait_until="networkidle")
 
         
         
@@ -35,4 +38,29 @@ def test_menu_links():
         assert links.nth(1).get_attribute("href") == "actualites.html"
         assert links.nth(2).get_attribute("href") == "membres.html"
         
+        browser.close()
+
+
+def test_menu_redirections():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+
+        # Charger la page principale
+        page.goto(BASE_URL, wait_until="networkidle")
+
+        # Lien 1 : Accueil
+        page.click("nav ul li a:text('Accueil')")
+        assert page.url == f"{BASE_URL}/index.html"
+
+        # Lien 2 : Actualités
+        page.click("nav ul li a:text('Actualités')")
+        assert page.url == f"{BASE_URL}/actualites.html"
+        assert page.locator("h1:has-text('Actualités')").is_visible()
+
+        # Lien 3 : Membres du Bureau
+        page.click("nav ul li a:text('Membres du Bureau')")
+        assert page.url == f"{BASE_URL}/membres.html"
+        assert page.locator("h1:has-text('Membres du Bureau')").is_visible()
+
         browser.close()
